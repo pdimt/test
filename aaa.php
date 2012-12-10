176c9f0ca2ef1b48dbb47b565d07aefd45f4f008
Dmitriy <puahdmit@ya.ru>
2012-12-10 16:03:38 +0300

    aaa

---
+++ aaa.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: user
+ * Date: 10.12.12
+ * Time: 16:03
+ * To change this template use File | Settings | File Templates.
+ */
+class aaa
+{
+
+}
